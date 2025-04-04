c1a264b6c9063f61b335bcd2b2c376d253b907c1
Murilo Maciel Rodrigues <murilomacielbr@gmail.com>
2025-04-04 10:32:39 -0300

    exercicio de tabuada

--- main.java
+++
@@ -1,8 +0,0 @@
-class Main {
-    public static void main(String[] args) {
-        int num1, num2;
-        num1 = 10;
-        num2 = 15;
-        System.out.println(num1 + " + " + num2 + " = " + (num1+num2) );
-    }
-}
